1884627e9d70c071baabfc5c79233168a7a7ceca
Park JeaHyun <jea5158@gmail.com>
2024-07-31 23:56:27 +0900

    first commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,6 @@
+import MainComponent from './MainComponent';
+
+const Layout = (props: any) => {
+	return <MainComponent />;
+};
+export default Layout;
---
+++ src/components/MainComponent.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const MainComponent = () => {
+  return (
+    <>
+      <h1>메인 컴포넌트입니다!</h1>
+    </>
+  );
+};
+export default MainComponent;
